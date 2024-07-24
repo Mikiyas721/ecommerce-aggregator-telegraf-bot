021884e0d9ac698ae860169554623465e1c633bb
Mikiyas721 <Mikiyas721@gmail.com>
2024-07-24 12:39:09 +0300

    Debug

--- src/modules/user/presentation/handlers/keyboard/feedback_keyboard_handlers.ts
+++ src/modules/user/presentation/handlers/keyboard/feedback_keyboard_handlers.ts
@@ -1,6 +1,8 @@
 import {TelegrafContext} from "../../../../../common/utils/telegraf_types/context_types";
+import {CommonHandlers} from "../../../../../common/presentation/handlers/common_handlers";
 
 export class FeedbackKeyboardHandlers {
     static async cancel(ctx: TelegrafContext) {
+        return CommonHandlers.sendMainMenuMessage(ctx)
     }
 }
